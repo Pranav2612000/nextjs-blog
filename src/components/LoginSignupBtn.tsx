ecbb47068bfa95b9805cde78b419f268808e96e9
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-01-30 20:12:53 +0530

    fix: update Login/Signup button to display corresponding text when active

--- src/components/LoginSignupBtn.tsx
+++ src/components/LoginSignupBtn.tsx
@@ -2,7 +2,7 @@ const LoginSignupBtn = (props) => {
   return (
     <>
       {props.isDataValid ? (
-        <button className="py-5 w-full text-center font-sfprobold bg-green-700 text-white">Sign up</button>
+        <button className="py-5 w-full text-center font-sfprobold bg-green-700 text-white">{props.text}</button>
         ) : (
         <button className="py-5 w-full text-center font-sfprobold bg-white text-gray-500 login-signup-submit-btn">{props.text}</button>
       )}
